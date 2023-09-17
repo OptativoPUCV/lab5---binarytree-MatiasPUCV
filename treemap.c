e46555614f28d63312211fe67e2d1752c57625d5
Replit user <>
2023-09-17 23:25:30 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -52,29 +52,29 @@ TreeNode* searchTreeNode(TreeMap* tree, TreeNode* node, void* key)
   if (node == NULL)
     return NULL;
 
-  if (is_equal(tree, node->pair->key, key))
+  if (is_equal(tree, node->pair->key, key) == 1)
     return node;
 
-  if (tree->lower_than(node->pair->key, key))
+  if (tree->lower_than(node->pair->key, key) == 1)
     return searchTreeNode(tree, node->left, key);
 
-  if (!tree->lower_than(node->pair->key, key))
+  if (tree->lower_than(node->pair->key, key) != 1)
     return searchTreeNode(tree, node->right, key);
 
   // Error
   return NULL;
 }
 
-void insertNode(TreeNode* node, TreeNode* new)
+void insertNode(TreeMap* tree, TreeNode* node, TreeNode* new)
 {
   if (node == NULL)
     node = new;
 
-  if (node->pair->key < new->pair->key)
-    insertNode(node->left, new);
+  if (tree->lower_than(node->pair->key < new->pair->key) == 1)
+    insertNode(tree, node->left, new);
 
-  if (node->pair->key > new->pair->key)
-    insertNode(node->right, new);
+  if (tree->lower_than(node->pair->key, new->pair->key) != 1)
+    insertNode(tree, node->right, new);
 }
 
 void insertTreeMap(TreeMap *tree, void *key, void *value)
@@ -88,7 +88,7 @@ void insertTreeMap(TreeMap *tree, void *key, void *value)
 
   TreeNode* new = createTreeNode(key, value);
 
-  insertNode(tree->root, new);
+  insertNode(tree, tree->root, new);
   tree->current = new;
 
 }
