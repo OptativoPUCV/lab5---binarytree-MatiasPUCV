76753e2ab3fcac18de4ef4f6bb0306f1c9635d7b
Replit user <>
2023-09-18 00:28:16 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -57,10 +57,10 @@ TreeNode *searchTreeNode(TreeMap *tree, TreeNode *node, void *key) {
     return node;
 
   else if (tree->lower_than(node->pair->key, key) == 1)
-    return searchTreeNode(tree, node->left, key);
-
-  else if (tree->lower_than(node->pair->key, key) != 1)
     return searchTreeNode(tree, node->right, key);
+
+  //else if (tree->lower_than(node->pair->key, key) != 1)
+  //  return searchTreeNode(tree, node->right, key);
 
   // Error
   return NULL;
